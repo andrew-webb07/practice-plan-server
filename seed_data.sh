81e3811c7727d921d83a6b5f73b1f89c2c40ee31
Andrew Webb <webba@ihopu.org>
2021-09-08 15:56:43 -0500

    created auth view with login and register functions

---
+++ seed_data.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+rm -rf practiceplanapi/migrations
+rm db.sqlite3
+python manage.py makemigrations practiceplanapi
+python manage.py migrate
+python manage.py loaddata users
+python manage.py loaddata tokens
+python manage.py loaddata players
+python manage.py loaddata categories
+python manage.py loaddata exercises
+python manage.py loaddata practiceplans
+python manage.py loaddata plan_exercises
+python manage.py loaddata sessions
