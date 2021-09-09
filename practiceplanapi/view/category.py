269c7d22f0307a4896ac4e774f3e8d43614d5c6c
Andrew Webb <webba@ihopu.org>
2021-09-09 10:58:20 -0500

    added player view

--- practiceplanapi/view/category.py
+++ practiceplanapi/view/category.py
@@ -6,7 +6,6 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers
 from practiceplanapi.models import Category, Player
-from django.db.models import Q
 
 class CategoryView(ViewSet):
     """Practice Plan Categories"""
