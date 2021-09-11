36225148b9cb7a40f8d12784939a66c75bcacbb6
Andrew Webb <webba@ihopu.org>
2021-09-11 10:27:23 -0500

    restricted categories to user and public users

--- practiceplanapi/models/category.py
+++ practiceplanapi/models/category.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.db import models
+from django.db.models.deletion import CASCADE
 
 class Category(models.Model):
+    player = models.ForeignKey("Player", on_delete=CASCADE)
     label = models.CharField(max_length=50)
